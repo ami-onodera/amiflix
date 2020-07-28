fea30051c36d1a51729a587866c933d4d574820e
Ami Onodera <onodera.camila@gmail.com>
2020-07-28 16:32:07 +0200

    header e footer

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from '../Menu';
+import Footer from '../Footer';
+
+function PageDefault() {
+  return (
+
+    <div>
+      <Menu />
+        Conteúdo mágico
+      <Footer />
+    </div>
+
+    );
+}
+
+export default PageDefault;
