ab8ac24403b6b1f127d51e7133a1ab0b5dfa32e4
Ami Onodera <onodera.camila@gmail.com>
2020-08-03 17:07:24 +0200

    aula 3

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -5,10 +5,10 @@ import PageDefault from '../../../components/PageDefault';
 function CadastroVideo() {
   return (
     <PageDefault>
-      <h1>Register a new Video</h1>
+      <h1>Cadastro de Video</h1>
 
       <Link to="/cadastro/categoria">
-        Register a Category
+        Cadastrar Categoria
       </Link>
     </PageDefault>
   )
